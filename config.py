0e7d2ce2460ce4f9e7ee033f5137d62e2ea41a4c
nicklisoft <nick.lisoft@gmail.com>
2021-04-30 22:34:21 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "1723594707:AAGjQYwLiMtKLLwZhcVlajt76ZgRbn9IGHI"
+exchanges = {
+    'доллар': 'usd',
+    'евро': 'eur',
+    'рубль': 'rub'
+}
+
+API_access_key = "8b429f7f00f62a87ff05a9472d340592"
+
+website = 'http://api.exchangeratesapi.io/v1/latest'
---
+++ test.py
@@ -0,0 +1,3 @@
+from extensions import Convertor
+res = Convertor.get_price(['рубль', 'доллар', 10])
+print(res)
